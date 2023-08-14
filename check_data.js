b2f9b8cd1547cf917f4edd67da3c87d5635ac39a
MarcMouries <Marc@Mouries.net>
2023-08-14 18:08:31 -0400

    check json file

--- check_data.js
+++ check_data.js
@@ -1,11 +1,29 @@
-require("./user_data.js");
+const fs = require('fs').promises;
 
-console.log( "Number of users = " + user_data.length )
+async function main() {
+    const FILE_NAME = 'users.json';
+    const user_data = await readJSONFile(FILE_NAME);
 
-user_data.forEach((user, index) => 
-{
-    /* ... */ 
-    const user_index = (++index +"").padStart(2, '0');
-    const user_name  = user.full_name.padEnd(16, ' ');
-    console.log( "User #" + user_index  +  " : " + user_name +  ", " + user.mobile_phone )
-})
+    console.log("Number of users =", user_data.length);
+
+    user_data.forEach((user, index) => {
+        const user_index = (++index + "").padStart(2, '0');
+        const user_name = user.full_name.padEnd(16, ' ');
+        console.log("User #" + user_index + " : " + user_name + ", " + user.mobile_phone);
+    });
+}
+
+async function readJSONFile(filename) {
+    try {
+        const data = await fs.readFile(filename, 'utf-8');
+        //console.log("readJSONFile: data", data);
+
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading the file:', error);
+    }
+}
+
+main().catch(error => {
+    console.error('Error in main:', error);
+});
